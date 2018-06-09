be1d618028bb81364fa7ac3886e1ac477329f345
EC2 Default User <ec2-user@ip-172-31-31-165.us-east-2.compute.internal>
2018-06-09 22:52:02 +0000

    add image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_015608) do
+ActiveRecord::Schema.define(version: 2018_06_09_210748) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_06_07_015608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "chef_id"
+    t.string "picture"
   end
 
 end

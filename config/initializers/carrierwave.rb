feb42585352d64003d078d5fb6f8514314f9352c
jpalms <joey.palmieri10@gmail.com>
2018-06-15 04:06:45 +0000

    added fog AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
 
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required

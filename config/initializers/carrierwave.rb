9e4cc0a6426f92ec5ba9975fb1e69ba546406058
jpalms <joey.palmieri10@gmail.com>
2018-06-15 04:00:38 +0000

    added fog AWS v2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,5 +8,6 @@ if Rails.env.production?
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_public     = false
   end
 end

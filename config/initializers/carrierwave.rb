71e101ab0e5330d431a75e630f62c726115adb69
jpalms <joey.palmieri10@gmail.com>
2018-06-16 01:39:00 +0000

    updating carrierwave region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-    :region =>                'us-east-2'
+    region:                'us-east-2'
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
   config.fog_public     = false

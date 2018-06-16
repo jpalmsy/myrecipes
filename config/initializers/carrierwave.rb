d9b039adce572ea9f7370598ea4293fa82a543fa
jpalms <joey.palmieri10@gmail.com>
2018-06-16 01:18:22 +0000

    updating carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 
+  config.fog_provider = 'fog/aws' 
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required

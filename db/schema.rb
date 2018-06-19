1a9d2ba5f8a609268e512aaefa1a782a20e4b800
jpalms <joey.palmieri10@gmail.com>
2018-06-19 00:34:10 +0000

    add admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_16_182721) do
+ActiveRecord::Schema.define(version: 2018_06_19_000118) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_16_182721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "ingredients", force: :cascade do |t|
